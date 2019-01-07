1850859eb6423bb0127634d1488b6d4e639010fc
gitSnake <GitSnake@users.noreply.github.com>
2019-01-07 12:28:59 -0500

    function to set cookie

--- 3-JavaScript (Raw)/2-js-cookie/cookie.js
+++ 3-JavaScript (Raw)/2-js-cookie/cookie.js
@@ -1,6 +1,7 @@
-// create a function that sorees the name of the visitor in a cookie variable
+// function to set a cookie (function stores name of visitor)
 function setCookie(cname, cvalue, exdays) {
   let d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
