0b53af63395bcdf97be157d713f84b8fc68ca06a
gitSnake <GitSnake@users.noreply.github.com>
2019-01-07 12:30:50 -0500

    edit comments

--- 3-JavaScript (Raw)/2-js-cookie/cookie.js
+++ 3-JavaScript (Raw)/2-js-cookie/cookie.js
@@ -1,7 +1,9 @@
-// function to set a cookie (function stores name of visitor)
+// function to Set a cookie (function stores name of visitor)
 function setCookie(cname, cvalue, exdays) {
   let d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+// function to Get a cookie (function to retreive existing cookie)
