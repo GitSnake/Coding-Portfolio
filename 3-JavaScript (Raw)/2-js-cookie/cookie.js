bd5f17acf7990b7402f9b61f783547677c4a35e8
gitSnake <GitSnake@users.noreply.github.com>
2019-01-07 02:19:31 -0500

    Added file path to cookie

--- 3-JavaScript (Raw)/2-js-cookie/cookie.js
+++ 3-JavaScript (Raw)/2-js-cookie/cookie.js
@@ -1,2 +1,2 @@
 // created cookie
-document.cookie = "username = John Doe; expires = Tues 1 Jan 2030 12:00:00 UTC";
+document.cookie = "username = John Doe; expires = Tues 1 Jan 2030 12:00:00 UTC; path=/";
