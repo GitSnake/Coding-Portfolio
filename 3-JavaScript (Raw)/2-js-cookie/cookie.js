58bca1b82ad9cc81d4e3c3ec8ce478b91c0c4d6b
gitSnake <GitSnake@users.noreply.github.com>
2019-01-08 01:35:32 -0500

    Added check a cookie function

--- 3-JavaScript (Raw)/2-js-cookie/cookie.js
+++ 3-JavaScript (Raw)/2-js-cookie/cookie.js
@@ -22,3 +22,16 @@ function getCookie(cname) {
   }
   return "";
 }
+
+// function to Check a cookie (function to check for existing cookies)
+function checkCookie() {
+  let username = getCookie("username");
+  if (username !=""){
+    alert("Welcome again " + username);
+  } else {
+    username = prompt("Please enter your name:", "");
+    if (username != "" && username != null) {
+      setCookie("username", username, 365);
+    }
+  }
+}
