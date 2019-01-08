9fe4de95bd83b1ffbe528588bd4adf9b8dda661a
gitSnake <GitSnake@users.noreply.github.com>
2019-01-08 01:22:42 -0500

    Added get cookie function

--- 3-JavaScript (Raw)/2-js-cookie/cookie.js
+++ 3-JavaScript (Raw)/2-js-cookie/cookie.js
@@ -7,3 +7,17 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 // function to Get a cookie (function to retreive existing cookie)
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+}
